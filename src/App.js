783a8fae57853737c68ace106bdcf7060508c97e
Arturo Mendivil <60411196+artmen1516@users.noreply.github.com>
2022-05-18 12:27:12 -0700

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,23 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
 
-import { BrowserRouter, Routes, Route} from 'react-router-dom';
-import './App.css';
-import 'bootstrap/dist/css/bootstrap.min.css';
-
-import Home from './Components/Home';
-import Formulario from './Components/Formulario';
-import Membresia from './Components/Membresia';
-import NavBar from './UI/NavBar';
+import Home from "./Components/Home";
+import Formulario from "./Components/Formulario";
+import Membresia from "./Components/Membresia";
+import NavBar from "./UI/NavBar";
+import Footer from "./UI/Footer";
 
 function App() {
   return (
     <BrowserRouter>
-    <NavBar  />
+      <NavBar />
       <Routes>
-        <Route element={<Home />} path='/' />
-        <Route element={<Formulario />} path='/formulario' />
-        <Route element={<Membresia />} path='/membresia' />
+        <Route element={<Home />} path="/" />
+        <Route element={<Formulario />} path="/formulario" />
+        <Route element={<Membresia />} path="/membresia" />
       </Routes>
-    
+      <Footer />
     </BrowserRouter>
   );
 }
