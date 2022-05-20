cf88c21fd796744e25d30de00d4b6c05cdbcb9fc
Denisse Herrera <denher11@gmail.com>
2022-05-20 10:36:11 -0700

    Update App.js

--- src/App.js
+++ src/App.js
@@ -2,9 +2,9 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 
-import Home from "./Components/Home";
+{/*import Home from "./Components/Home";
 import Formulario from "./Components/Formulario";
-import Membresia from "./Components/Membresia";
+import Membresia from "./Components/Membresia";*/}
 import NavBar from "./UI/NavBar";
 import Footer from "./UI/Footer";
 
