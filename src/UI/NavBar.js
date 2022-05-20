2569ba7c33f9474b4226050ee11433a60175d81b
Denisse Herrera <denher11@gmail.com>
2022-05-20 16:33:19 -0700

    fix link decoration

--- src/UI/NavBar.js
+++ src/UI/NavBar.js
@@ -1,5 +1,6 @@
 import { Navbar, Container, Nav } from "react-bootstrap";
 import { Link } from "react-router-dom";
+import "./Footer.scss";
 
 
 export default function NavBar() {
@@ -11,9 +12,9 @@ export default function NavBar() {
             <Navbar.Toggle aria-controls="responsive-navbar-nav" />
             <Navbar.Collapse id="responsive-navbar-nav">
             <Nav className="ms-auto">
-                
-                <Link to="/membresia"><Nav.Link href="/membresia">Membresia</Nav.Link></Link>
-                <Link to="/formulario"><Nav.Link href="/formulario">Solicitud</Nav.Link></Link>
+             
+                <Link className="noLine" to="/membresia"><Nav.Link href="/membresia">Membresia</Nav.Link></Link>
+                <Link className="noLine" to="/formulario"><Nav.Link href="/formulario">Solicitud</Nav.Link></Link>
             </Nav>
             </Navbar.Collapse>
         </Container>     
