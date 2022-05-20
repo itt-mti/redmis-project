ee091fff7017cc9463fe4a7bc5fdf9711691354c
Arturo Mendivil <60411196+artmen1516@users.noreply.github.com>
2022-05-19 18:05:08 -0700

    Update App.js

--- src/App.js
+++ src/App.js
@@ -12,7 +12,7 @@ function App() {
   return (
     <>
     <div className="Site">
-    <BrowserRouter>
+    <BrowserRouter basename="/redmis-project">
       <NavBar />
       <div className="Site-Content">
       <Routes>
