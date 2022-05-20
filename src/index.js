959709e3eef24adf0ee7eac0686622152da32b38
Denisse Herrera <denher11@gmail.com>
2022-05-20 10:30:58 -0700

    Update index.js

--- src/index.js
+++ src/index.js
@@ -3,6 +3,9 @@ import ReactDOM from 'react-dom/client';
 import { HashRouter, Routes, Route } from 'react-router-dom';
 import './index.css';
 import App from './App';
+import Home from "./Components/Home";
+import Formulario from "./Components/Formulario";
+import Membresia from "./Components/Membresia";
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
