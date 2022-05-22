b8d750a771e6d5cf25b951b0d20c71315dc6fa79
mirnh <m21210041@tectijuana.edu.mx>
2022-05-21 23:45:55 -0700

    changes to send information to backend using supabase

--- src/Components/ImageUpload.js
+++ src/Components/ImageUpload.js
@@ -5,13 +5,12 @@ export default class ImageUpload extends Component {
   constructor(props) {
     super(props);
     this.selectFile = this.selectFile.bind(this);
-    this.upload = this.upload.bind(this);
 
     this.state = {
       currentFile: undefined,
       previewImage: undefined,
-      progress: 0,
       message: "",
+      fileName: undefined,
 
       imageInfos: [],
     };
@@ -26,77 +25,45 @@ export default class ImageUpload extends Component {
   }
 
   selectFile(event) {
-    this.setState({
-      currentFile: event.target.files[0],
-      previewImage: URL.createObjectURL(event.target.files[0]),
-      progress: 0,
-      message: ""
-    });
-  }
+    const file = event.target.files[0];
 
-  upload() {
+    const fileExt = file.name.split(".").pop();
+    const fileName = `${Math.random()}.${fileExt}`;
+    const filePath = `${fileName}`;
+
     this.setState({
-      progress: 0,
+      currentFile: file,
+      previewImage: URL.createObjectURL(file),
+      message: "",
+      fileName: filePath,
     });
 
-    UploadService.upload(this.state.currentFile, (event) => {
-      this.setState({
-        progress: Math.round((100 * event.loaded) / event.total),
-      });
-    })
-      .then((response) => {
-        this.setState({
-          message: response.data.message,
-        });
-        return UploadService.getFiles();
-      })
-      .then((files) => {
-        this.setState({
-          imageInfos: files.data,
-        });
-      })
-      .catch((err) => {
-        this.setState({
-          progress: 0,
-          message: "Could not upload the image!",
-          currentFile: undefined,
-        });
-      });
+    this.props.selectFile(file, filePath);
   }
 
   render() {
-    const {
-      
-      previewImage,
-    
-      message,
-
-    } = this.state;
+    const { previewImage, message } = this.state;
 
     return (
       <div>
-        <div className="row"> 
-
-        {previewImage && (
-          <div>
-            <img className="preview" src={previewImage} alt="" />
-          </div>
-        )}
+        <div className="row">
+          {previewImage && (
+            <div>
+              <img className="preview" src={previewImage} alt="" />
+            </div>
+          )}
           <div className="col-8 mt-2">
             <label className="btn btn-default p-0">
               <input type="file" accept="image/*" onChange={this.selectFile} />
             </label>
           </div>
-
         </div>
 
         {message && (
           <div className="alert alert-secondary mt-3" role="alert">
             {message}
-          </div> 
+          </div>
         )}
-
-     
       </div>
     );
   }
