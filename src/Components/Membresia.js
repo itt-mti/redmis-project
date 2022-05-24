ccdaf14a8abac9c288d3829e48b254654c0c306d
Denisse Herrera <denher11@gmail.com>
2022-05-23 17:33:05 -0700

    git

--- src/Components/Membresia.js
+++ src/Components/Membresia.js
@@ -4,11 +4,7 @@ import Footer from "../UI/Footer";
 
 
 function Membresia() {
-<<<<<<< HEAD
     return (
-=======
-   return (
->>>>>>> 9b6daa9fc9364d82c6e2e08f80aa0bd4893c57f1
         <>
         <div className="Site">
              
