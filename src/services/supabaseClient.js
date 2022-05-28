16983424245630b02483b75e9d3b4ce0ba42aa28
mirnh <m21210041@tectijuana.edu.mx>
2022-05-27 20:39:52 -0700

    adds supabase cross origin header

--- src/services/supabaseClient.js
+++ src/services/supabaseClient.js
@@ -3,23 +3,21 @@ import { createClient } from "@supabase/supabase-js";
 const supabaseUrl = process.env.REACT_APP_SUPABASE_URL;
 const supabaseAnonKey = process.env.REACT_APP_SUPABASE_ANON_KEY;
 
-export const supabase = createClient(supabaseUrl, supabaseAnonKey);
+export const supabase = createClient(supabaseUrl, supabaseAnonKey, {
+  headers: { "Access-Control-Allow-Origin": "*" },
+});
 
 export const supabaseInsertEntidadAsync = async (values) => {
-  return await supabase
-    .from("redmis_entidad")
-    .insert([values]);
+  return await supabase.from("redmis_entidad").insert([values]);
 };
 
 export const supabaseReadEntidadAsync = async (values) => {
-  return await supabase
-    .from("redmis_entidad")
-    .select('*');
+  return await supabase.from("redmis_entidad").select("*");
 };
 
 export const supabaseUploadFileAsync = async (fileName, file, fileType) => {
   const result = await supabase.storage.from(fileType).upload(fileName, file);
-  console.log({file,fileName,fileType,result});
+  console.log({ file, fileName, fileType, result });
 };
 
 export const supabaseDownloadFileAsync = async (
